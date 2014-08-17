39dc3a412011a0a08595a695c20222812399539e
Marcel Wijnands <marcel@shynet.nl>
2014-08-17 23:42:24 +0200

    Added CacheKeyGenerator tests.

---
+++ SimpleCache.Tests/Internal/CacheKeyGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using XperiCode.SimpleCache.Internal;
+using XperiCode.SimpleCache.Tests.Test;
+
+namespace XperiCode.SimpleCache.Tests.Internal
+{
+    [TestClass]
+    public class CacheKeyGeneratorTests
+    {
+        [TestMethod]
+        public void ShouldGenerateCacheKeyPrefixedWithGenericTypeParameterFullName()
+        {
+            string personTypeFullName = typeof(Person).FullName;
+            string key = "FindPerson";
+
+            string expectedCacheKey = string.Format("[{0}][{1}]", personTypeFullName, key);
+            string cacheKey = CacheKeyGenerator.GenerateCacheKey<Person>(key);
+
+            Assert.AreEqual(expectedCacheKey, cacheKey);
+        }
+    }
+}
